122968f6066161cb826227b5e0bbb7973054f6ee
BasilDhilloo <bdhilloo@gmail.com>
2022-09-22 20:06:54 -0400

    Updates for Contact Component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
 import { RiMessengerLine } from 'react-icons/ri'
@@ -8,16 +8,39 @@ import emailjs from 'emailjs-com';
 
 
 const Contact = () => {
-    let initialState = '';
-    const [{ name, email, message }, setState] = useState(initialState)
-    const [statusMessage, setStatusMessage] = useState("");
+    // const [{ name, email, message }, setState] = useState(initialState)
+    const [statusMessage, setStatusMessage] = useState(false);
+    // let messageSuccess;
 
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setState((prevState) => ({ ...prevState, [name]: value }))
-    }
-    const clearState = () => setState({ ...initialState })
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target
+    //     setState((prevState) => ({ ...prevState, [name]: value }))
+    // }
+    // const clearState = () => setState({ ...initialState })
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        // document.title = `You clicked ${count} times`;
+
+        setTimeout(() => {
+            setStatusMessage(false)
+        }, 5000);
+
+        if (statusMessage) {
+            console.log("Here goes message")
+            // messageSuccess = 
+            // console.log(messageSuccess)
+        }
+
+
+
+    }, [statusMessage]);
+
+
+
+
 
     const form = useRef();
     const sendEmail = (e) => {
@@ -26,21 +49,30 @@ const Contact = () => {
         emailjs.sendForm('service_4zohetf', 'template_26dtvna', form.current, 'f83ndX6S-gP05kLbe')
             .then((result) => {
                 console.log(result.text);
-                setStatusMessage("Email sent success");
-                clearState();
+                setStatusMessage(true);
+                // if (result.text === "OK") {
+                //     messageSuccess = <p> This is message </p>
+                // }
+                // clearState();
+
             }, (error) => {
                 console.log(error.text);
-                setStatusMessage(`${error.text} happened`);
+                // setStatusMessage(`${error.text} happened`);
             });
 
         e.target.reset()
     };
+
+
+    // { statusMessage && (< div className='messageSucess'> This is message </div>) }
     return (
         <section id="contact">
             <h5></h5>
             <h2></h2>
 
             <div className="container contact__container">
+                {statusMessage && <div className='messageSucess'>Message Sent Successfully!</div>}
+
                 <div className="contact__options">
                     <article className="contact__option">
                         <MdOutlineEmail className='contact__option__icon' />
@@ -63,14 +95,15 @@ const Contact = () => {
                 </div>
 
                 <form ref={form} onSubmit={sendEmail}>
-                    <small className='message__success'> {statusMessage}</small>
                     <input type="text" name="name" placeholder='Enter Your Full Name' required />
                     <input type="email" name="email" placeholder='Enter Your Email Address' required />
                     <textarea name="message" rows="7" placeholder='Type your message here!' required></textarea>
                     <button type='submit' className='btn btn-primary'> Send Message</button>
                 </form>
-            </div>
-        </section>
+
+            </div >
+
+        </section >
     )
 }
 
